a1331208095f334dfe07dbf55a225a70b741c2f0
tejaTech339 <tejaTech339@gmail.com>
2024-01-11 18:04:29 +0530

    JDBC Auth with Spring Boot

---
+++ src/main/java/in/jdbcsecurity/SbSecurityApiJdbcApplication.java
@@ -0,0 +1,13 @@
+package in.jdbcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityApiJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityApiJdbcApplication.class, args);
+	}
+
+}
